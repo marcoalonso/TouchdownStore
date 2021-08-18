3473a896e79978e7b93165c0d3542b263ec9a427
marcoalonso <marcoalonsorota@hotmail.com>
2021-08-18 13:57:29 -0500

    Getting-started

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by marco rodriguez on 18/08/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
